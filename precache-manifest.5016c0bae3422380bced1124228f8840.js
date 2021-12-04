fc80c03a86dc7bee5cd2c2c7cb26b43282db1ec0
amir23reza <amir.reza.haghverdi.khu@gmail.com>
2021-12-03 20:30:56 -0500

    Updates

---
+++ precache-manifest.5016c0bae3422380bced1124228f8840.js
@@ -0,0 +1,226 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ce4ace605f7ac0587b4ea49992ea7cf",
+    "url": "/amirrezahaghverdi/index.html"
+  },
+  {
+    "revision": "c117ce5dd28ff625a2a6",
+    "url": "/amirrezahaghverdi/static/css/2.6ceced1d.chunk.css"
+  },
+  {
+    "revision": "ece05532393c4066ea1e",
+    "url": "/amirrezahaghverdi/static/css/main.314ffcc6.chunk.css"
+  },
+  {
+    "revision": "c117ce5dd28ff625a2a6",
+    "url": "/amirrezahaghverdi/static/js/2.90f5fd99.chunk.js"
+  },
+  {
+    "revision": "ece05532393c4066ea1e",
+    "url": "/amirrezahaghverdi/static/js/main.6b6a78ba.chunk.js"
+  },
+  {
+    "revision": "c9a85909428292d3162d",
+    "url": "/amirrezahaghverdi/static/js/runtime~main.6e4950eb.js"
+  },
+  {
+    "revision": "39b2c3031be6b4ea96e2e3e95d307814",
+    "url": "/amirrezahaghverdi/static/media/Roboto-Bold.39b2c303.woff2"
+  },
+  {
+    "revision": "dc81817def276b4f21395f7ea5e88dcd",
+    "url": "/amirrezahaghverdi/static/media/Roboto-Bold.dc81817d.woff"
+  },
+  {
+    "revision": "e31fcf1885e371e19f5786c2bdfeae1b",
+    "url": "/amirrezahaghverdi/static/media/Roboto-Bold.e31fcf18.ttf"
+  },
+  {
+    "revision": "ecdd509cadbf1ea78b8d2e31ec52328c",
+    "url": "/amirrezahaghverdi/static/media/Roboto-Bold.ecdd509c.eot"
+  },
+  {
+    "revision": "3b813c2ae0d04909a33a18d792912ee7",
+    "url": "/amirrezahaghverdi/static/media/Roboto-Light.3b813c2a.woff"
+  },
+  {
+    "revision": "46e48ce0628835f68a7369d0254e4283",
+    "url": "/amirrezahaghverdi/static/media/Roboto-Light.46e48ce0.ttf"
+  },
+  {
+    "revision": "69f8a0617ac472f78e45841323a3df9e",
+    "url": "/amirrezahaghverdi/static/media/Roboto-Light.69f8a061.woff2"
+  },
+  {
+    "revision": "a990f611f2305dc12965f186c2ef2690",
+    "url": "/amirrezahaghverdi/static/media/Roboto-Light.a990f611.eot"
+  },
+  {
+    "revision": "4d9f3f9e5195e7b074bb63ba4ce42208",
+    "url": "/amirrezahaghverdi/static/media/Roboto-Medium.4d9f3f9e.eot"
+  },
+  {
+    "revision": "574fd0b50367f886d359e8264938fc37",
+    "url": "/amirrezahaghverdi/static/media/Roboto-Medium.574fd0b5.woff2"
+  },
+  {
+    "revision": "894a2ede85a483bf9bedefd4db45cdb9",
+    "url": "/amirrezahaghverdi/static/media/Roboto-Medium.894a2ede.ttf"
+  },
+  {
+    "revision": "fc78759e93a6cac50458610e3d9d63a0",
+    "url": "/amirrezahaghverdi/static/media/Roboto-Medium.fc78759e.woff"
+  },
+  {
+    "revision": "2751ee43015f9884c3642f103b7f70c9",
+    "url": "/amirrezahaghverdi/static/media/Roboto-Regular.2751ee43.woff2"
+  },
+  {
+    "revision": "30799efa5bf74129468ad4e257551dc3",
+    "url": "/amirrezahaghverdi/static/media/Roboto-Regular.30799efa.eot"
+  },
+  {
+    "revision": "ba3dcd8903e3d0af5de7792777f8ae0d",
+    "url": "/amirrezahaghverdi/static/media/Roboto-Regular.ba3dcd89.woff"
+  },
+  {
+    "revision": "df7b648ce5356ea1ebce435b3459fd60",
+    "url": "/amirrezahaghverdi/static/media/Roboto-Regular.df7b648c.ttf"
+  },
+  {
+    "revision": "7500519de3d82e33d1587f8042e2afcb",
+    "url": "/amirrezahaghverdi/static/media/Roboto-Thin.7500519d.woff"
+  },
+  {
+    "revision": "94998475f6aea65f558494802416c1cf",
+    "url": "/amirrezahaghverdi/static/media/Roboto-Thin.94998475.ttf"
+  },
+  {
+    "revision": "954bbdeb86483e4ffea00c4591530ece",
+    "url": "/amirrezahaghverdi/static/media/Roboto-Thin.954bbdeb.woff2"
+  },
+  {
+    "revision": "dfe56a876d0282555d1e2458e278060f",
+    "url": "/amirrezahaghverdi/static/media/Roboto-Thin.dfe56a87.eot"
+  },
+  {
+    "revision": "ecee62913ed20bfefda5b9a289ccb4ce",
+    "url": "/amirrezahaghverdi/static/media/about.ecee6291.jpg"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/amirrezahaghverdi/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/amirrezahaghverdi/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "7571d953ff46825af65affc4da0e74a3",
+    "url": "/amirrezahaghverdi/static/media/asprin_web-min.7571d953.png"
+  },
+  {
+    "revision": "ab577429d43740789278c6910d80209d",
+    "url": "/amirrezahaghverdi/static/media/biblio1.ab577429.png"
+  },
+  {
+    "revision": "ecb84f0a2dee269ad5cc13ee6de8ed17",
+    "url": "/amirrezahaghverdi/static/media/bimarNegar-min.ecb84f0a.png"
+  },
+  {
+    "revision": "8afaff96065a716b11d0c736193454bc",
+    "url": "/amirrezahaghverdi/static/media/clusters.8afaff96.png"
+  },
+  {
+    "revision": "f51efa2b1e6dd5cb80ba2acccf62fe58",
+    "url": "/amirrezahaghverdi/static/media/docTel-min.f51efa2b.png"
+  },
+  {
+    "revision": "2ef8ba3410dcc71578a880e7064acd7a",
+    "url": "/amirrezahaghverdi/static/media/fa-brands-400.2ef8ba34.woff"
+  },
+  {
+    "revision": "5bf145531213545e03ff41cd27df7d2b",
+    "url": "/amirrezahaghverdi/static/media/fa-brands-400.5bf14553.svg"
+  },
+  {
+    "revision": "5e2f92123d241cabecf0b289b9b08d4a",
+    "url": "/amirrezahaghverdi/static/media/fa-brands-400.5e2f9212.woff2"
+  },
+  {
+    "revision": "98b6db59be947f563350d2284fc9ea36",
+    "url": "/amirrezahaghverdi/static/media/fa-brands-400.98b6db59.ttf"
+  },
+  {
+    "revision": "a7b95dbdd87e0c809570affaf366a434",
+    "url": "/amirrezahaghverdi/static/media/fa-brands-400.a7b95dbd.eot"
+  },
+  {
+    "revision": "427d721b86fc9c68b2e85ad42b69238c",
+    "url": "/amirrezahaghverdi/static/media/fa-regular-400.427d721b.woff"
+  },
+  {
+    "revision": "5eb754ab7dbd2fee562360528db4c3c0",
+    "url": "/amirrezahaghverdi/static/media/fa-regular-400.5eb754ab.svg"
+  },
+  {
+    "revision": "65b9977aa23185e8964b36eddbce7a20",
+    "url": "/amirrezahaghverdi/static/media/fa-regular-400.65b9977a.ttf"
+  },
+  {
+    "revision": "dcce4b7fbd5e895561e18af4668265af",
+    "url": "/amirrezahaghverdi/static/media/fa-regular-400.dcce4b7f.eot"
+  },
+  {
+    "revision": "e6257a726a0cf6ec8c6fec22821c055f",
+    "url": "/amirrezahaghverdi/static/media/fa-regular-400.e6257a72.woff2"
+  },
+  {
+    "revision": "418dad87601f9c8abd0e5798c0dc1feb",
+    "url": "/amirrezahaghverdi/static/media/fa-solid-900.418dad87.woff2"
+  },
+  {
+    "revision": "46e7cec623d8bd790d9fdbc8de2d3ee7",
+    "url": "/amirrezahaghverdi/static/media/fa-solid-900.46e7cec6.eot"
+  },
+  {
+    "revision": "49279363201ed19a840796e05a74a91b",
+    "url": "/amirrezahaghverdi/static/media/fa-solid-900.49279363.svg"
+  },
+  {
+    "revision": "a7140145ebaaf5fb14e40430af5d25c4",
+    "url": "/amirrezahaghverdi/static/media/fa-solid-900.a7140145.woff"
+  },
+  {
+    "revision": "ff8d9f8adb0d09f11d4816a152672f53",
+    "url": "/amirrezahaghverdi/static/media/fa-solid-900.ff8d9f8a.ttf"
+  },
+  {
+    "revision": "4787e886507d025d4e4feb9ea3e9a0b0",
+    "url": "/amirrezahaghverdi/static/media/lawyer-min.4787e886.png"
+  },
+  {
+    "revision": "74727c772ccfe3ad91c58e1da9c5a89a",
+    "url": "/amirrezahaghverdi/static/media/logo.74727c77.png"
+  },
+  {
+    "revision": "2372f25c3b161f6559b9c783bcc0673b",
+    "url": "/amirrezahaghverdi/static/media/mirtaheri.2372f25c.jpg"
+  },
+  {
+    "revision": "dd60abac7d1a9a15996c2ff9fadc0381",
+    "url": "/amirrezahaghverdi/static/media/pop.dd60abac.png"
+  },
+  {
+    "revision": "f5b6af44ccb2f93a406e573b3af795ef",
+    "url": "/amirrezahaghverdi/static/media/salehi.f5b6af44.jpg"
+  },
+  {
+    "revision": "6fd7dfc731c922e125c4d06e813a36ec",
+    "url": "/amirrezahaghverdi/static/media/skim.6fd7dfc7.png"
+  },
+  {
+    "revision": "2c69cdcacee078cd8ba528b5e0985118",
+    "url": "/amirrezahaghverdi/static/media/taheri.2c69cdca.jpg"
+  }
+]);
